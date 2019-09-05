168a766f1b54c1a379a70432b19d797d98a01038
Septa Kusumaningtyas <septakusumaningtyas@gmail.com>
2019-09-05 14:18:12 +0700

    menambah

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"App on Start",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"App on Stop",Toast.LENGTH_SHORT).show();
     }
 }
